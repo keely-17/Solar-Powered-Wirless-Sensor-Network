b1bc8a72b4db90e33ec141f5eb492afbea2452b8
Keely <keelymdamore@gmail.com>
2024-11-03 20:34:11 -0500

    added html and css placeholders

--- python/dht22_test.py
+++ python/dht22_test.py
@@ -1,18 +1,15 @@
 import Adafruit_DHT
 import time
-import board
-import busio
 
 
 sensor = Adafruit_DHT.DHT22
 gpio = 4
-
-
+wait = 5
 humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-print(f"hum: {humidity:.1f}")
-
-
+temperature = (9/5) * temperature + 32
 
 if humidity is not None and temperature is not None: 
-    temperature = (9/5) * temperature + 32
-    print(f" Temp: {temperature:.1f}F")
+    print(f"Temp: {temperature:.1f}F")
+    print(f"hum: {humidity:.1f}")
+    time.sleep(wait)
+    print("goodbye")
